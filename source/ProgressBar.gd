2e47f217eebb51249bf63ccc5120d6ac27792bd2
CorvatoCafeto <emma.fs92@gmail.com>
2019-01-18 16:28:53 -0300

    Breakpoint bar

--- source/ProgressBar.gd
+++ source/ProgressBar.gd
@@ -1,2 +1,16 @@
 extends ProgressBar
 
+var timer = Timer.new()
+
+func _ready():
+	timer.set_wait_time(1)
+	timer.set_timer_process_mode(Timer.TIMER_PROCESS_IDLE)
+	timer.set_one_shot(false)
+	timer.connect("timeout", self, "update")
+	
+	add_child(timer)
+	timer.start()
+
+func update():
+	if get_child(0).wait_time !=0:
+		self.value += 1	
