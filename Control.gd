05f5fbe1e8c31ceedcde5a8c3e44bbf0b6e2b72d
CorvatoCafeto <emma.fs92@gmail.com>
2019-01-18 11:06:39 -0300

    Minor story scene upgrade

--- Control.gd
+++ Control.gd
@@ -4,6 +4,10 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene(Global.Game)
 
-func _process(delta):
-	if $Read_time.time_left == 0:
-		get_tree().change_scene(Global.Game)
+#func _process(delta):
+#	if $Read_time.time_left == 0:
+#		get_tree().change_scene(Global.Game)
+
+func _on_Continue_pressed():
+	get_tree().change_scene(Global.Game)
+
