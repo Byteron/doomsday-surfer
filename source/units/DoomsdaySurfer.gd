960d2b18e5001afeee75048e128c57c4414f24ae
Aaron Winter <winter.aaron93@gmail.com>
2019-01-19 16:06:10 +0100

    implemented idle/surfing animations for doomsday surfer

--- source/units/DoomsdaySurfer.gd
+++ source/units/DoomsdaySurfer.gd
@@ -14,7 +14,11 @@ func surfer(loc):
 		if last_quadrant != loc.quadrant:
 			last_quadrant = loc.quadrant
 			$QuadrantFreezeTimer.start()
+			play("surfing")
+			offset = Vector2(16, 16)
 			emit_signal("surfer_moved", loc.quadrant)
 
 func _on_QuadrantFreezeTimer_timeout():
 	emit_signal("timeout")
+	play("idle")
+	offset = Vector2(25, -25)
